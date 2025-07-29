e0d72d2a7bed2fcc7548d4e90edf52d1627ca3ff
AKER\anhkc <anhk.ce191266@gmail.com>
2025-07-29 15:00:12 +0700

    Cleaned redundant code, changed path of "find all shops managed by this group"

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -15,8 +15,14 @@ import { StoreRO } from './ro/store.ro';
 export class StoreController {
     constructor(private readonly StoreService: StoreService) { }
 
+    @Get('groups')
+    @ApiOperation({ summary: 'Retrieve a list of all group-type stores' })
+    @ApiResponse({ status: 200, description: 'List of group-type stores.', type: [Store] })
+    async filterGroupOnlyList(): Promise<StoreRO[]> {
+        return await this.StoreService.findAllGroups();
+    }
 
-    @Get('groups/:uuid/shops')
+    @Get(':uuid/shops')
     @ApiOperation({ summary: 'Retrieve all child shops of a specific parent group' })
     @ApiParam({ name: 'uuid', description: 'The display ID of the parent group', type: String })
     @ApiResponse({ status: 200, description: 'List of child shops.', type: [Store] })
@@ -25,28 +31,7 @@ export class StoreController {
     async findChildShopsOfThisGroup(@Param() dto: UuidDto): Promise<StoreRO[]> {
         return await this.StoreService.findChildShopsOfThisGroup(dto.uuid);
     }
-
-    // @Get('groups/:uuid/admin')
-    // @ApiOperation({ summary: 'Retrieve all stores which are under the same admin as this group' })
-    // @ApiParam({ name: 'uuid', description: 'The display ID of the group', type: String })
-    // @ApiResponse({
-    //     status: 200,
-    //     description: 'List of stores which are under the same admin as this group',
-    //     type: [Store]
-    // })
-    // @ApiResponse({ status: 404, description: 'Group not found.' })
-    // @ApiResponse({ status: 400, description: 'Invalid UUID format.' })
-    // async findDeliverableStores(@Param() dto: UuidDto): Promise<Store[]> {
-    //     return await this.StoreService.findStoresUnderTheSameAdminAsThisGroup(dto.uuid);
-    // }
-
-    @Get('groups')
-    @ApiOperation({ summary: 'Retrieve a list of all group-type stores' })
-    @ApiResponse({ status: 200, description: 'List of group-type stores.', type: [Store] })
-    async filterGroupOnlyList(): Promise<StoreRO[]> {
-        return await this.StoreService.findAllGroups();
-    }
-
+    
     @Get(':uuid')
     @ApiOperation({ summary: 'Retrieve a single store by its display ID' })
     @ApiParam({ name: 'uuid', description: 'The display ID of the store', type: String })
